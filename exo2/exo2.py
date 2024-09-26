d9dc61aef2bc7ade9938dd9b8253d24052949fae
Nouha209 <nouhaila.gargouz@centrale-casablanca.ma>
2024-09-26 13:08:20 +0200

    Update exo2.py

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -8,6 +8,12 @@ Examples:
     solution('abc', 'd') # returns false
 """
 
+
+def solution(string, ending):
+
+
+    return string.endswith(ending)
+
 """
 Create unit test using those cases:
 fixed_tests_True = (
@@ -28,3 +34,42 @@ fixed_tests_False = (
     ( "spam",    "eggs"  )
 )
 """
+import unittest
+
+class TestSolution(unittest.TestCase):
+
+
+    def test_fixed_tests_true(self):
+
+
+        fixed_tests_True = (
+            ("samurai", "ai"),
+            ("ninja", "ja"),
+            ("sensei", "i"),
+            ("abc", "abc"),
+            ("abcabc", "bc"),
+            ("fails", "ails"),
+        )
+        for string, ending in fixed_tests_True:
+            with self.subTest(string=string, ending=ending):
+                self.assertTrue(solution(string, ending))
+
+
+    def test_fixed_tests_false(self):
+
+        
+        fixed_tests_False = (
+            ("sumo", "omo"),
+            ("samurai", "ra"),
+            ("abc", "abcd"),
+            ("ails", "fails"),
+            ("this", "fails"),
+            ("spam", "eggs"),
+        )
+        for string, ending in fixed_tests_False:
+            with self.subTest(string=string, ending=ending):
+                self.assertFalse(solution(string, ending))
+
+# Running the tests
+if __name__ == "__main__":
+    unittest.main()
